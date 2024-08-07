a27d48bee70337be8d3df64c11dbafd648fa7df0
sidd1398 <useful0220@naver.com>
2024-08-08 00:04:21 +0900

    Add files via upload

--- wooparoo_cross.py
+++ wooparoo_cross.py
@@ -11,7 +11,7 @@ st.title("가상 크로스")
 
 # 사용자 입력 받기
 left = st.text_input("왼쪽 우파루:")
-left = st.text_input("오른쪽 우파루:")
+right = st.text_input("오른쪽 우파루:")
 
 # 버튼을 눌렀을 때 실행되는 코드
 if st.button("Submit"):
