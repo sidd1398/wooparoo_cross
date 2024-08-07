e0ed5fa87f00ecaa15a8f4b77f4c49ca9e2263de
sidd1398 <useful0220@naver.com>
2024-08-07 23:48:32 +0900

    Add files via upload

---
+++ wooparoo_cross.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+# 애플리케이션 제목
+st.title("가상 크로스")
+
+# 사용자 입력 받기
+left = st.text_input("왼쪽 우파루:")
+left = st.text_input("오른쪽 우파루:")
+
+# 버튼을 눌렀을 때 실행되는 코드
+if st.button("Submit"):
+    st.write(f"Hello, {left} and {right}!")
+
+
+# In[ ]:
+
+
+
+
