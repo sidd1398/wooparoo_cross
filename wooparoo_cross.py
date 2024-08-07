7754800a51a9b217b9890865b95bb74708ff411d
sidd1398 <useful0220@naver.com>
2024-08-08 00:43:34 +0900

    Add files via upload

--- wooparoo_cross.py
+++ wooparoo_cross.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
+import json
 import streamlit as st
+import pandas as pd
+
+# JSON 파일을 읽어옵니다.
+with open('sorted_wooparoo_all_data.json', 'r', encoding='utf-8') as file:
+    sorted_data = json.load(file)
 
 # 애플리케이션 제목
 st.title("가상 크로스")
@@ -13,9 +19,24 @@ st.title("가상 크로스")
 left = st.text_input("왼쪽 우파루:")
 right = st.text_input("오른쪽 우파루:")
 
+
 # 버튼을 눌렀을 때 실행되는 코드
 if st.button("Submit"):
-    st.write(f"Hello, {left} and {right}!")
+    # 일치하는 url_index를 찾고, 해당 data를 출력합니다.
+    found = False
+    for item in sorted_data:
+        if item['url_index'] == input_array:
+            found = True
+            #st.write(f"왼쪽 우파루: {input_array[0]}, 오른쪽 우파루: {input_array[1]}")                
+            break
+
+    if not found:
+        print(f"입력한 이름의 우파루가 없습니다: {input_array}")
+    else:
+        # pandas DataFrame으로 변환하고 열 이름 설정
+        df = pd.DataFrame(item['data'], columns=["결과 우파루", "확률(%)"])
+        df.index = df.index + 1
+        print(df)
 
 
 # In[ ]:
